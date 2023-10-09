0d5276aaf3580ae5624b3140d2c69e4537e55c54
Deathbaron <user@deathbaron.org>
2023-10-09 12:45:26 +0200

    initial commit

---
+++ arkime/start-arkimeviewer.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+until curl -sS "http://$ES_HOST:$ES_PORT/_cluster/health?wait_for_status=yellow" > /dev/null 2>&1
+do
+    echo "Waiting for ES to start"
+    sleep 3
+done
+
+echo "ES started..."
+
+COUNT=1
+ATTEMPTS=10
+until [[ -f "/eve/eve.json" ]] || [[ $COUNT -gt $ATTEMPTS ]]
+do
+    echo "Waiting for Suricata to populate eve.json ( $(( COUNT++ ))/${ATTEMPTS} )"
+    sleep 3
+done
+if [[ $COUNT -gt $ATTEMPTS ]]
+then
+    echo "Skipping Suricata eve.json..."
+else
+    echo "Suricata populated eve.json..."
+fi
+
+# set runtime environment variables
+export ARKIME_ELASTICSEARCH="http://"$ES_HOST":"$ES_PORT
+$ARKIMEDIR/db/db.pl $ARKIME_ELASTICSEARCH init --ifneeded
+$ARKIMEDIR/bin/arkime_add_user.sh $ARKIME_ADMIN_USERNAME admin $ARKIME_ADMIN_PASSWORD --admin --email --remove --packetSearch --createOnly
+$ARKIMEDIR/bin/arkime_add_user.sh $ARKIME_WEBAUTH_TOKEN webauth $ARKIME_WEBAUTH_TOKEN --admin --webauthonly --email --remove --packetSearch --createOnly
+
+echo "Starting Arkime capture in the background..."
+cd $ARKIMEDIR
+exec $ARKIMEDIR/bin/capture -c $ARKIMEDIR/etc/config.ini -R /data/ --monitor --skip 2>&1 &
+echo " Default credentials"
+echo " >> user: $ARKIME_ADMIN_USERNAME"
+echo " >> password: $ARKIME_ADMIN_PASSWORD"
+
+echo "Launch viewer..."
+cd $ARKIMEDIR/viewer
+$ARKIMEDIR/bin/node $ARKIMEDIR/viewer/viewer.js -c $ARKIMEDIR/etc/config.ini 2>&1
---
+++ pcap-mover/pcap-mover.py
@@ -0,0 +1,76 @@
+#!/bin/env python3
+# pip3 install asyncinotify 
+# set CAPTURE_DIRECTORY for watched folder
+
+from pathlib import Path
+from asyncinotify import Inotify, Mask
+from os import environ
+import asyncio
+import logging
+
+
+logging.basicConfig(format='[%(levelname)s] %(message)s', encoding='utf-8', level=logging.INFO)
+
+watch_dir = Path(environ.get("CAPTURE_DIRECTORY","/capture"))
+
+suricata_dir = Path(watch_dir, "suricata")
+arkime_dir = Path(watch_dir, "arkime")
+
+background_tasks = set()
+
+def create_background_task(coro):
+    task = asyncio.create_task(coro)
+    background_tasks.add(task)
+    task.add_done_callback(background_tasks.discard)
+
+async def add_suricata_task(path: Path):
+    create_background_task(arkime_delayed(path))
+    suricata_path = Path(suricata_dir, path.name)
+    if not suricata_path.exists():
+        logging.info(f"Adding {path.as_posix()} to Suricata.")
+        suricata_path.hardlink_to(path)
+
+async def add_arkime_task(path: Path):
+    arkime_path = Path(arkime_dir, path.name)
+    if not arkime_path.exists():
+        logging.info(f"Adding {path.as_posix()} to Arkime.")
+        arkime_path.hardlink_to(path)
+
+async def arkime_delayed(path: Path):
+    await asyncio.sleep(60)
+    logging.info(f"Arkime timer expired for {path.as_posix()}.")
+    await add_arkime_task(path)
+
+def add_missing():
+    sub = watch_dir.glob('*')
+    for path in sub:
+        if path.suffix == ".pcap" and path.is_file():
+            suricata_path = Path(suricata_dir, path.name)
+            arkime_path = Path(arkime_dir, path.name)
+            if not suricata_path.exists() and not arkime_path.exists():
+                create_background_task(add_suricata_task(path))
+
+async def main():
+    logging.info("Starting.")
+    suricata_dir.mkdir(parents=True, exist_ok=True)
+    arkime_dir.mkdir(parents=True, exist_ok=True)
+    with Inotify() as inotify:
+        inotify.add_watch(watch_dir, Mask.CLOSE)
+        inotify.add_watch(suricata_dir, Mask.DELETE)
+        add_missing()
+        async for event in inotify:
+            if event.path:
+                path = event.path
+                watch = event.watch
+                if path.suffix == ".pcap":
+                    if watch.path == watch_dir and path.is_file():
+                        create_background_task(add_suricata_task(path))
+                    elif watch.path == suricata_dir:
+                        watch_path = Path(watch_dir, path.name)
+                        create_background_task(add_arkime_task(watch_path))
+    asyncio.gather(*background_tasks);
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    logging.info("Shutting down.")
