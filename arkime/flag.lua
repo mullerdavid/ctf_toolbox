a54131aa4257e5d6c4b373324eea66f5d9fb1836
Deathbaron <user@deathbaron.org>
2023-10-13 20:02:37 +0200

    adding flag regex tagger for arkime

---
+++ arkime/flag.lua
@@ -0,0 +1,25 @@
+local ENV_FLAG = os.getenv("FLAG_REGEX")
+local REGEX_FLAG = MolochData.pcre_create(ENV_FLAG)
+
+print("Flag LUA plugin loaded with FLAG_REGEX:", ENV_FLAG)
+
+local function parser_flag(session, str, direction)
+    data = MolochData.new(str)
+    local matched = data:pcre_match(REGEX_FLAG)
+    if matched then
+        session:add_tag("FLAG")
+        if direction == 0 then -- request
+            session:add_tag("FLAG_IN")
+        else
+            session:add_tag("FLAG_OUT")
+        end
+        
+    end
+end
+
+function register_parser(session, str, direction)
+    session:register_parser(parser_flag)
+end
+
+MolochSession.register_tcp_classifier("flag", 0, "", "register_parser")
+MolochSession.register_udp_classifier("flag", 0, "", "register_parser")
