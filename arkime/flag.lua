6315ce4d0661a368b60a85662f0ede88db13848f
Deathbaron <user@deathbaron.org>
2024-09-16 00:24:47 +0200

    bugfix flag format lua parser, using arkime instead old moloch

--- arkime/flag.lua
+++ arkime/flag.lua
@@ -1,10 +1,10 @@
 local ENV_FLAG = os.getenv("FLAG_REGEX")
-local REGEX_FLAG = MolochData.pcre_create(ENV_FLAG)
+local REGEX_FLAG = ArkimeData.pcre_create(ENV_FLAG)
 
 print("Flag LUA plugin loaded with FLAG_REGEX:", ENV_FLAG)
 
 local function parser_flag(session, str, direction)
-    data = MolochData.new(str)
+    data = ArkimeData.new(str)
     local matched = data:pcre_match(REGEX_FLAG)
     if matched then
         session:add_tag("FLAG")
@@ -21,5 +21,5 @@ function register_parser(session, str, direction)
     session:register_parser(parser_flag)
 end
 
-MolochSession.register_tcp_classifier("flag", 0, "", "register_parser")
-MolochSession.register_udp_classifier("flag", 0, "", "register_parser")
+ArkimeSession.register_tcp_classifier("flag", 0, "", "register_parser")
+ArkimeSession.register_udp_classifier("flag", 0, "", "register_parser")
